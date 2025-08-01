bbc2a6d2c57c26363cb70127bba45494379a86c7
polar-sudo-debug <polargtyt2@gmail.com>
2025-07-31 23:57:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,11 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxyMiddleware = require('./proxy');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Proxy middleware options
-const options = {
-    target: 'https://discord.com/', // Change this to the target website
-    changeOrigin: true,
-    onProxyReq: (proxyReq, req, res) => {
-        // Modify headers if needed
-        proxyReq.setHeader('origin', 'null');
-        proxyReq.setHeader('x-requested-with', 'XMLHttpRequest');
-    },
-    onProxyRes: (proxyRes, req, res) => {
-        // Modify response headers if needed
-        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    },
-    onError: (err, req, res) => {
-        res.writeHead(500, {
-            'Content-Type': 'text/plain'
-        });
-        res.end('Something went wrong. And we are reporting a custom error message.');
-    }
-};
-
-// Create the proxy
-const exampleProxy = createProxyMiddleware(options);
-
-// Use the proxy
-app.use('/proxy', exampleProxy);
+// Use the proxy middleware
+app.use('/proxy', proxyMiddleware);
 
 // Serve the HTML file
 app.use(express.static('public'));
